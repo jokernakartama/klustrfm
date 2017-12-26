1ee72ab4040b51e3a4c99cf3a2862707b01c5d9a
zzap <eat.yummies@gmail.com>
2017-12-26 22:40:33 +0300

    Added actions and reducer for service panel

---
+++ source/js/ducks/servicePanel.js
@@ -0,0 +1,109 @@
+import { serviceMap } from '~/api/index.js'
+import { putToken, checkToken } from '~/utilities/tokenBag'
+
+var initialState = {
+}
+
+for (var service in serviceMap) {
+  initialState[service] = {
+    name: service,
+    mounted: false 
+  }
+}
+export const SERVICE_MOUNT = 'servicepanel::service_mount'
+export const SERVICE_UNMOUNT = 'servicepanel::service_unmount'
+export const SERVICE_AUTHORIZATION_FAILED = 'servicepanel::service_authorization_failed'
+
+export function mountService (serviceName) {
+  return {
+    type: SERVICE_MOUNT,
+    payload: serviceName
+  }
+}
+
+export function unmountService (serviceName) {
+  return {
+    type: SERVICE_UNMOUNT,
+    payload: serviceName
+  }
+}
+
+/**
+ * Connects service according token bag data
+ * @param serviceName {string}
+ * @returns {boolean}
+ */
+export function connectService (serviceName) {
+  return function (dispatch) {
+    var Service = serviceMap[serviceName]
+    var tokenData = checkToken(serviceName)
+    /**
+     * there we got checkToken as promise
+     * on success save token and dispatch
+     */
+    if (Service && tokenData) {
+      Service.saveTokenData(tokenData)
+      dispatch(mountService(serviceName))
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+/**
+ * Connects service and adds new token data
+ * recieved from an authorization window
+ * @param serviceName {string}
+ */
+export function addService (serviceName) {
+  return function (dispatch) {
+    var Service = serviceMap[serviceName]
+    if (Service) {
+      dispatch(authorizationStart())
+      Service.openAuthWindow((rawData, win) => {
+        try {
+          var parsed = JSON.parse(rawData)
+          Service.getToken(parsed, (data) => {
+            Service.saveTokenData(data, () => {
+              putToken(serviceName, data)
+            })
+            dispatch(mountService(serviceName))
+          }, (data) => {
+            /* cannot get token */
+            console.log('ERROR: ', data)
+          })
+        } catch (e) {
+          if (e); // any error handler
+          console.log(e)
+        }
+        win.close()
+        dispatch(authorizationEnd())
+      })
+    }
+  }
+}
+
+const actionsMap = {
+  [SERVICE_MOUNT]: (state, action) => {
+    var service = Object.assign({}, state[action.payload])
+    service.mounted = true
+    return { 
+      ...state,
+      [action.payload]: service
+    }
+  },
+  [SERVICE_UNMOUNT]: (state, action) => {
+    var service = Object.assign({}, state[action.payload])
+    service.mounted = false
+    return { 
+      ...state,
+      [action.payload]: service
+    }
+  },
+}
+
+export default function reducer(state = initialState, action = {}) {
+  const fn = actionsMap[action.type];
+  return fn ? fn(state, action) : state;
+}
